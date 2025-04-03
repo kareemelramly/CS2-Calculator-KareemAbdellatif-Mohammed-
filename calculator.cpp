1532ecad53547500353fc071c32bf47942a536c1
kareemelramly <kareem.koki125@gmail.com>
2025-04-03 03:38:02 +0200

    addition, substraction, multipication, and division were added

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include "calculator.h"
+#include<bits\stdc++.h>
+using namespace std;
+int addition(int num1, int num2){
+return(num1+num2);
+}
+int substraction(int num1, int num2){
+    return(num1-num2);
+}
+int multipication(int num1, int num2){
+    return(num1*num2);
+}
+double division(int num1, int num2){
+    return(double(num1)/num2);
+}
---
+++ calculator.h
@@ -0,0 +1,6 @@
+#include<bits\stdc++.h>
+using namespace std;
+int addition(int, int );
+int substraction(int,int);
+int multipication(int,int);
+double division(int,int);
